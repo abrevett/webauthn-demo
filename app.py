7801e5c5cae8c8a314981031548f858f56b86d78
Ainsworth Brevett <abrevett@udel.edu>
2022-01-17 15:31:47 -0500

    Changed route paths and logic for WebAuthn integration

--- app.py
+++ app.py
@@ -8,17 +8,32 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/auth', methods=['POST'])
+# This route registers a new user
+@app.route('/api/register', methods=['POST'])
 def auth():
+    # Casting POST data to a Python dict
     login = json.loads( request.get_data().decode("utf-8") )
     log.info( "POST at /auth data: " + str(login) )
+    # Check db if the user alredy exists
+    if db.users.count_documents( {"username":login['username']} ) > 0:
+        return "User already exists"
     # SUPER UNSAFE way to add the username and password to the MongoDB database
     result = db.users.insert_one( login )
     if result != None: return str(result)
     else: return "Auth Fail"
 
+# This route authenticates an existing user
+@app.route('/api/login', methods=['POST'])
+def login():
+    verify = json.loads( request.get_data().decode("utf-8") )
+    log.info( "POST at /api/login: " + str(login) )
+    # Check db for the user existing
+    if db.users.count_documents( {"username":verify['username']} ) < 0:
+        return "User doesn't exist"
+    return "Auth OK"
+
 logging.basicConfig(level=os.environ.get("LOGLEVEL","INFO"))
-log = logging.getLogger(__file__)
+log = logging.getLogger("webauthn")
 log.info("Starting web server")
 
 log.info("Connecting to MongoDB")
