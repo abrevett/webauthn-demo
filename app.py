0f39fd5093f257194e98626d54795a6e7d3372a0
Ainsworth Brevett <abrevett@udel.edu>
2022-01-18 23:00:04 -0500

    Changed api routes to accept GET and POST

--- app.py
+++ app.py
@@ -9,28 +9,30 @@ def index():
     return render_template('index.html')
 
 # This route registers a new user
-@app.route('/api/register', methods=['POST'])
+@app.route('/api/register', methods=['GET','POST'])
 def auth():
-    # Casting POST data to a Python dict
-    login = json.loads( request.get_data().decode("utf-8") )
-    log.info( "POST at /auth data: " + str(login) )
-    # Check db if the user alredy exists
-    if db.users.count_documents( {"username":login['username']} ) > 0:
-        return "User already exists"
-    # SUPER UNSAFE way to add the username and password to the MongoDB database
-    result = db.users.insert_one( login )
-    if result != None: return str(result)
-    else: return "Auth Fail"
+    if request.method == 'POST':
+        # Casting POST data to a Python dict
+        login = json.loads( request.get_data().decode("utf-8") )
+        log.info( "POST at /auth data: " + str(login) )
+        # Check db if the user alredy exists
+        if db.users.count_documents( {"username":login['username']} ) > 0:
+            return "User already exists"
+        # SUPER UNSAFE way to add the username and password to the MongoDB database
+        result = db.users.insert_one( login )
+        if result != None: return str(result)
+        else: return "Auth Fail"
 
 # This route authenticates an existing user
-@app.route('/api/login', methods=['POST'])
+@app.route('/api/login', methods=['GET','POST'])
 def login():
-    verify = json.loads( request.get_data().decode("utf-8") )
-    log.info( "POST at /api/login: " + str(login) )
-    # Check db for the user existing
-    if db.users.count_documents( {"username":verify['username']} ) < 0:
-        return "User doesn't exist"
-    return "Auth OK"
+    if request.method == 'POST':
+        verify = json.loads( request.get_data().decode("utf-8") )
+        log.info( "POST at /api/login: " + str(login) )
+        # Check db for the user existing
+        if db.users.count_documents( {"username":verify['username']} ) < 0:
+            return "User doesn't exist"
+        return "Auth OK"
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL","INFO"))
 log = logging.getLogger("webauthn")
