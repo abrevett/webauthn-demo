c3d149f36290c01314424ca6fcc52a70a004c5f5
Ainsworth Brevett <abrevett@udel.edu>
2022-02-15 22:07:41 -0500

    Changed API to use promises for Regsitration callback

--- static/webauth.js
+++ static/webauth.js
@@ -1,85 +1,63 @@
 /**
- * OLD CODE
-var sendForm = function(){
-	var user = document.querySelector("#user").value;
-	var pass = document.querySelector("#pass").value;
-	var ajaxreq = new XMLHttpRequest();
-	ajaxreq.onreadystatechange = function(){
-		document.querySelector("#authok").innerText = this.responseText;
-	};
-	ajaxreq.open('POST', 'api/register', true);
-	ajaxreq.send(JSON.stringify({
-		username: user,
-		password: pass
-	}));
-};
-*/
-
-/**
  * Registers a new user on the website
  *
  * Goes to the TPM and fetches a public key to 
  * give to the server to register
+ *
+ * To guarantee that all data is sent and is valid, this is a Promise chain
 */
 var registerUser = function(){
 	var email = document.querySelector("#register-email").value;
 	var user = document.querySelector("#register-user").value;
 	var resp = null;
 	// Sending our email and username to check if the user is taken
-	var ajaxreq = new XMLHttpRequest();
-	ajaxreq.onreadystatechange = function(){
-		resp = JSON.parse(this.responseText);
-	};
-	ajaxreq.open('GET', 'api/register', true);
-	ajaxreq.send(JSON.stringify({
-		email: email,
-		username: user
-	}));
-
+	fetch('/api/register', {
+		method: 'PUT',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify( {email: email, username: user} )
+	}).then(r=>r.json())
+		.then(resp => {
+			if('error' in resp){ console.log(resp);	return;	}
+			if( !('publicKey' in resp) ){ console.log(resp); return; }
 	// We must take the response object and properly cast its datatypes
 	// to ArrayBuffer types
 	var id = resp.publicKey.user.id;
 	resp.publicKey.challenge = bufferDecode( resp.publicKey.challenge );
-	resp.publicKey.user.id = bufferDecode( resp.publicKey.user.id );
+	resp.publicKey.challenge = bufferDecode( resp.publicKey.user.id );
 	if(resp.publicKey.excludeCredentials) {
 		for(var i=0; i<resp.publicKey.excludeCredentials.length; i++){
 			resp.publicKey.excludeCredentials[i].id = bufferDecode( resp.publicKey.excludeCredentials[i].id );
 		}
 	}
-
 	// We directly use the response object from the api to send to the
 	// web browser, which will make a key pair on the TPM
-	var cred = null;
 	navigator.credentials.create({
 		publicKey: resp.publicKey
-	}).then( function(new_cred) {
-		cred = new_cred;
-	}).catch( function(err) {
-		console.info(err);
-	});
-
-	// We take the Array Objects of the Credential object and turn them
-	// into URL-safe Base64 to compress size
-	cred.rawId = bufferEncode( cred.rawId );
-	cred.response.attestationObject = bufferEncode( cred.response.attestationObject );
-	cred.response.clientDataJSON = bufferEncode( cred.response.clientDataJSON );
-
+	}).then( cred => {
+		// We take the Array Objects of the Credential object and turn them
+		// into URL-safe Base64 to compress size
+		cred.rawId = bufferEncode( cred.rawId );
+		cred.response.attestationObject = bufferEncode( cred.response.attestationObject );
+		cred.response.clientDataJSON = bufferEncode( cred.response.clientDataJSON );
 	// Finally, we send the credentials from the TPM back to the server
 	// to finish the registration process
-	ajaxreq = new XMLHttpRequest();
-	ajaxreq.onreadystatechange = function(){
-		resp = JSON.parse(this.responseText);
-	};
-	ajaxreq.open('POST', 'api/register', true);
-	ajaxreq.send(JSON.stringify({
-		email: email,
-		username: user,
-		id: id,
-		credential: cred
-	}));
-
-	// Now to just print the response just for fun
-	document.querySelector("#authok").innerText = resp;
+	fetch('/api/register', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify( {email: email, username: user, id: id, credential: cred} )
+	}).then(r=>r.json())
+		.then(resp => { 
+			// Now to just print the response just for fun
+			document.querySelector("#auth-ok").innerText = resp; 
+	// Catching the error from api/register POST
+		}).catch(err => { console.log(err); });
+	// Catching the error from navigator.credentials.create
+	}).catch( function(err) {
+		console.info(err);
+		});
+	// Catching the error from api/register PUT
+	}).catch(err => { console.log(err); 
+	});
 };
 
 /**
@@ -91,17 +69,19 @@ var registerUser = function(){
 */
 var loginUser = function(){
 	var email = document.querySelector("#login-email").value;
-	var resp = null;
 	// We send our email to verify an account exists and
 	// get the server challenge
-	var ajaxreq = new XMLHttpRequest();
-	ajaxreq.onreadystatechange = function(){
-		resp = JSON.parse(this.responseText);
-	};
-	ajaxreq.open('GET', 'api/login', true);
-	ajaxreq.send(JSON.stringify({
-		email: email
-	}));
+	fetch('/api/login', {
+		method: 'PUT',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify( {email: email} )
+	}).then(r=>r.json())
+		.then(dat => { resp = dat; })
+		.catch(err => { console.log(err); });
+	if(resp.error !== undefined){
+		console.log(resp.error);
+		return;
+	}
 
 	// We take our response Object and ensure the correct datatypes
 	resp.publicKey.challenge = bufferDecode( resp.publicKey.challenge );
@@ -127,18 +107,21 @@ var loginUser = function(){
 	cred.response.userHandle = bufferEncode( cred.response.userHandle );
 
 	// And give it to the server to verify our credentials
-	ajaxreq = new XMLHttpRequest();
-	ajaxreq.onreadystatechange = function(){
-		resp = JSON.parse(this.responseText);
-	};
-	ajaxreq.open('POST', 'api/login', true);
-	ajaxreq.send(JSON.stringify({
-		email: email,
-		credential: cred
-	}));
+	resp = null;
+	fetch('/api/register', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify( {email: email, credential: cred} )
+	}).then(r=>r.json())
+		.then(dat => { resp = dat; })
+		.catch(err => { console.log(err); });
+	if(resp.error !== undefined){
+		console.log(resp.error);
+		return;
+	}
 
 	// Again we display the response from the server
-	document.querySelector("#authok").innerText = resp;
+	document.querySelector("#auth-ok").innerText = resp;
 };
 
 
